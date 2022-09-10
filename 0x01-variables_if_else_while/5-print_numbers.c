fc9c0f14788db8777c0b1360311c0c354bdff769
JosiahMbithi001 <mbithishucks@gmail.com>
2022-09-09 17:47:02 -0700

    Print-Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d", n);
+	}
+	putchar('\n');
+
+	return (0);
+}
