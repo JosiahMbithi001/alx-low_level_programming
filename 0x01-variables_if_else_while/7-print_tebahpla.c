e9c2fd68600ed908a292f7869150a93dc65c5b4e
JosiahMbithi001 <mbithishucks@gmail.com>
2022-09-09 18:24:28 -0700

    Yes

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+
+char y;
+for (y = 'z'; y >= 'a'; y--)
+{
+putchar(y);
+}
+putchar('\n');
+
+return (0);
+}
